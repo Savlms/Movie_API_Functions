238e858b62a77864979b0878d6e6eb5366ea59cd
Savlms <valmarnas@gmail.com>
2024-01-28 14:40:35 -0800

    new update

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ class Movie {
 
 class User {
     constructor() {
-        this.name = this.random();
+        this.name = this.randomRealName();
     }
 
-    random() {
-        const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        let randomString = '';
-        for (let count = 0; count < 10; count += 1) {
-            const randomPosition = Math.floor(Math.random() * charset.length);
-            randomString += charset.substring(randomPosition, randomPosition + 1);
-        }    
-        return randomString;
+    randomRealName() {
+        const firstNames = ['Mara', 'Sommy', 'Uche', 'Ola', 'Michael', 'Olivia', 'David', 'Sophia'];
+        const lastNames = ['Nweke', 'Ike', 'Williams', 'Femi', 'Henry', 'Ekete', 'Chuks', 'Aneke'];
+
+        const randomFirstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+        const randomLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+
+        return `${randomFirstName} ${randomLastName}`;
     }
 }
 
 const randomUser = new User();
 
-console.log('\nUser has been generated with random username as: ', randomUser.name);
+console.log('\nUsername:', randomUser.name);
 
 class MovieStore {
     #Movies;
@@ -30,9 +30,9 @@ class MovieStore {
 
     constructor(title) {
         this.title = title;
-        this.#Movies = []; //movie list in the store
+        this.#Movies = [];
         this.#Rented = {
-            [randomUser.name]: [] // we only have one user with a randomly generated username as the name
+            [randomUser.name]: []
         };
     }
 
@@ -44,7 +44,7 @@ class MovieStore {
             movie,
             totalNumber
         };
-        // This is not considered when the movie already exists
+
         this.#Movies.push(_movie);
     }
 
@@ -77,15 +77,12 @@ class MovieStore {
             return 'Movie not found or not available';
         }
 
-        // this holds all the movies rented by our random user
         const userMovies = this.#Rented[randomUser.name];
-
-        // remove the movie with the provided title from the list of the user's movies
         this.#Rented[randomUser.name] = userMovies.filter((_movie) => _movie !== movie.movie.title.toLowerCase());
 
         this.#Movies.map((_movie) => {
             if (_movie.movie.title === title) {
-                _movie.totalNumber = _movie.totalNumber + 1; //increase of total number of available movies
+                _movie.totalNumber = _movie.totalNumber + 1;
                 return _movie;
             }
         });
@@ -100,10 +97,10 @@ class MovieStore {
 
 console.log('*******************A new Movie Store********************');
 const movieStore = new MovieStore('Joy of the Town');
-console.log('Successfully made a New Movie Store in Town');
+console.log('Welcome to Joy of the Town Movie Store');
 
 console.log('*******************Adding new movies to the store*******************\n');
-movieStore.addMovies({ title: 'Lift', totalNumber: 10 });
+movieStore.addMovies({ title: 'Lift', totalNumber: 13 });
 movieStore.addMovies({ title: '60 Minutes', totalNumber: 4 });
 movieStore.addMovies({ title: 'Wonka', totalNumber: 2 });
 movieStore.addMovies({ title: 'Home', totalNumber: 15 });
